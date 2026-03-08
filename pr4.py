54c58769c190b90c3e5b76645e39d7b6c575417f
Abhishek Angadi <153805526+Shadow3456rh@users.noreply.github.com>
2026-03-08 20:44:20 +0530

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -1,53 +1,74 @@
 import heapq
-graph = {'A':[('B', 25),('C',65)], 'B':[('D',35),('E',45)],'C':[('F',23),('G',0)],'D':[],'E':[],'F':[],'G':[]}
-import heapq
+graph = {
+    'A': [('B', 2), ('C', 3), ('D', 4)],
+    'B': [('E', 1), ('F', 3)],
+    'C': [('G', 4)],
+    'D': [('H', 2), ('I', 1)],
+    'E': [('J', 8)],
+    'F': [],
+    'G': [('L', 5)],
+    'H': [('M', 3)],
+    'I': [],
+    'J': [],
+    'K': [],
+    'L': [],
+    'M': []
+}
 
-def greedy_best_first_search(graph, start, goal):
-    visited = set()
+heuristic = {
+    'A': 7,
+    'B': 6,
+    'C': 5,
+    'D': 4,
+    'E': 6,
+    'F': 7,
+    'G': 3,
+    'H': 2,
+    'I': 4,
+    'J': 8,
+    'K': 9,
+    'L': 2,
+    'M': 0
+}
 
-    priority_queue = []
-    heapq.heappush(priority_queue, (0, start, [start]))
+def gbfs(start,goal,graph):
+    PQ=[(heuristic[start],start,[start])]
+    visited=set()
+    while PQ:
+        heuristics,node,path=heapq.heappop(PQ)
+        if node==goal:
+            return heuristics,path
+        if node not in visited:
+            visited.add(node)
+        
+        for neighbors,cost in graph[node]:
+            if neighbors not in visited:
+                heapq.heappush(PQ,(heuristic[neighbors], neighbors,path+[neighbors]))
 
-    while priority_queue:
-        heuristic, current_node, path = heapq.heappop(priority_queue)
+heur,pat=gbfs('A','M',graph)
+print(heur)
+print(pat)
 
-        if current_node == goal:
-            return path
+def astar(start,goal,graph):
+    PQ=[(heuristic[start],0,start,[start])]
+    visited={}
+    while PQ:
+        f,g,node,path=heapq.heappop(PQ)
+        if node==goal:
+            return f,path
+        
+        if node in visited and visited[node]<=g:
+            continue
+        
+        visited[node]=g
 
-        if current_node not in visited:
-            visited.add(current_node)
+        for neighbors,g_score in graph.get(node,[]):
+            new_g=g+g_score
+            new_f=new_g+heuristic[neighbors]
+            if neighbors not in visited:
+                heapq.heappush(PQ,(new_f,new_g,neighbors,path+[neighbors]))
 
-            for neighbor, h_value in graph[current_node]:
-                if neighbor not in visited:
-                    heapq.heappush(priority_queue, (h_value, neighbor, path + [neighbor]))
+f,path=astar('A','M',graph)
+print(f)
+print(path)
 
-    return None
-
-result = greedy_best_first_search(graph, 'A', 'G')
-print("Greedy Best First Search Path:", result)
-
-############A*###############
-def a_star_search(graph, start, goal):
-    visited = set()
-    priority_queue = []
-    heapq.heappush(priority_queue, (0, 0, start, [start]))
-
-    while priority_queue:
-        f_cost, g_cost, current_node, path = heapq.heappop(priority_queue)
-
-        if current_node == goal:
-            return path
-
-        if current_node not in visited:
-            visited.add(current_node)
-
-            for neighbor, cost in graph[current_node]:
-                if neighbor not in visited:
-                    new_g = g_cost + cost
-                    new_f = new_g + cost
-                    heapq.heappush(priority_queue, (new_f, new_g, neighbor, path + [neighbor]))
-
-    return None
-
-result = a_star_search(graph, 'A', 'G')
-print("A* Search Path:", result)
