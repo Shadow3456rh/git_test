647c7af2a0c292204c9a84b21045b1795d681616
Abhishek Angadi <153805526+Shadow3456rh@users.noreply.github.com>
2026-03-08 21:19:29 +0530

    Update pr2_waterjug.py

--- pr2_waterjug.py
+++ pr2_waterjug.py
@@ -1,37 +1,36 @@
 from collections import deque
-def water_jug_bfs(capA,capB,target):
-    queue=deque([(0,0,[(0,0)])])
+
+def bfs(capA,capB,goal):
+    queue=deque([(0,0,[0,0])])
     visited=set([0,0])
     while queue:
-        currA, currB, path = queue.popleft()
-        if(currA==target or currB==target):
+        currA,currB,path=queue.popleft()
+        if(currA==goal or currB==goal):
             return path
-        moves = [
+        moves=[
             (capA,currB),
             (currA,capB),
             (0,currB),
-            (currA, 0),
+            (currA,0),
             (
-                currA - min(currA, capB-currB),
-                currB + min(currA, capB-currB)
+                currA-min(currA,capB-currB),
+                currB+min(currA,capB-currB)
             ),
             (
-                currB - min(currB, capA-currA),
-                currA + min(currB, capA-currA)
+                currA+min(currB,capA-currA),
+                currB-min(currB,capA-currA)
             )
         ]
-
-        for nextA, nextB in moves:
+        for nextA,nextB in moves:
             if(nextA,nextB) not in visited:
                 visited.add((nextA,nextB))
-                queue.append((nextA,nextB, path+[(nextA,nextB)]))
-# Example
-result = water_jug_bfs(4, 3, 2)
 
-if isinstance(result, list):
-    print("Steps:")
-    for step in result:
-        print(step)
-    print("Minimum steps required =", len(result) - 1)
+                queue.append((nextA,nextB,path+[(nextA,nextB)]))
+
+result=bfs(4,3,2)
+
+if isinstance(result,list):
+    for i in result:
+        print(i)
 else:
     print(result)
