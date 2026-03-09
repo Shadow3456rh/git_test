adcbbda02001337da1f8bdcbe52033465e1dd9b6
Abhishek Angadi <153805526+Shadow3456rh@users.noreply.github.com>
2026-03-09 08:18:34 +0530

    Update pr4.py

--- pr4.py
+++ pr4.py
@@ -35,16 +35,15 @@ def gbfs(start,goal,graph):
     PQ=[(heuristic[start],start,[start])]
     visited=set()
     while PQ:
-        heuristics,node,path=heapq.heappop(PQ)
-        if node==goal:
-            return heuristics,path
+        heuristic_cost,node,path=heapq.heappop(PQ)
+        if(node==goal):
+            return heuristic_cost,path
         if node not in visited:
             visited.add(node)
+            for neighbors,cost in graph.get(node,[]):
+                if neighbors not in visited:
+                    heapq.heappush(PQ,(heuristic[neighbors],neighbors,path+[neighbors]))
         
-        for neighbors,cost in graph[node]:
-            if neighbors not in visited:
-                heapq.heappush(PQ,(heuristic[neighbors], neighbors,path+[neighbors]))
-
 heur,pat=gbfs('A','M',graph)
 print(heur)
 print(pat)
@@ -54,20 +53,20 @@ def astar(start,goal,graph):
     visited={}
     while PQ:
         f,g,node,path=heapq.heappop(PQ)
-        if node==goal:
+        if(node==goal):
             return f,path
         
         if node in visited and visited[node]<=g:
             continue
-        
+
         visited[node]=g
 
         for neighbors,g_score in graph.get(node,[]):
             new_g=g+g_score
             new_f=new_g+heuristic[neighbors]
-            if neighbors not in visited:
+            if neighbors not in visited or new_g<visited[neighbors]:
                 heapq.heappush(PQ,(new_f,new_g,neighbors,path+[neighbors]))
-
+    return None
 f,path=astar('A','M',graph)
 print(f)
 print(path)
